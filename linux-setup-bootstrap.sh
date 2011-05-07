103bd065b377a2726b02cb0c711306d581588b99
Ingy dot Net <ingy@ingy-x40.(none)>
2011-05-07 15:53:32 +1000

    first commit

---
+++ linux-setup-bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt-get install git
+sudo apt-get install vim
+sudo apt-get install exuberant-ctags
+
+pushd $HOME
+if [ ! -d ... ]; then
+    git clone git://github.com/ingydotnet/....git
+    ./.../...
+    echo 'sourceif /home/ingy/.bashrc../linux' > .bashrc../local
+fi
+popd
+
+exec bash
