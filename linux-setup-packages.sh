84b7a49dad942a510ade6e9029944d65b7a0c250
Ingy dot Net <ingy@ingy-x40.(none)>
2011-05-07 16:22:49 +1000

    add packages script

---
+++ linux-setup-packages.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt-get install git
+sudo apt-get install vim
+sudo apt-get install exuberant-ctags
+
+sudo apt-get install aptitude
+sudo apt-get install curl
+sudo apt-get install pidgin
+sudo apt-get install pidgin-otr
+sudo apt-get install skype
+sudo apt-get install xsel
+sudo apt-get install openvpn
+sudo apt-get install resolvconf
+sudo apt-get install perl-doc
