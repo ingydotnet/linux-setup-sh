b2c6dc362b0d546881662d7d5f5f419929f5b40c
Ingy dot Net <ingy@ingy-x40.(none)>
2011-05-07 16:47:59 +1000

    additions

--- linux-setup-perl.sh
+++ linux-setup-perl.sh
@@ -2,5 +2,8 @@
 
 curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 cpanm --sudo AYCABTU
+cpanm --sudo Module::Install
+cpanm --sudo Task::Plack
 cpanm --sudo YAML::XS
+cpanm --sudo YAML::Shell
 cpanm --sudo IO::All
